25af125fe8210a97f926b03c84fdd0d0bc9810a0
Langat Elisha Kipkoech <langatelisha09@gmail.com>
2022-08-19 07:37:45 +0300

    Add files via upload

---
+++ basics.kt
@@ -0,0 +1,148 @@
+package com.example.kotlinbasics
+
+fun main(){
+    //immutable variables
+    // TODO: add new functionality
+
+    /* multiline comments
+    comments
+    val myByte: Byte = 8 bits
+    val myShort: Short = 16 bits
+    val myInt: Int = 32 bits
+    val myLong: Long = 64 bits
+     */
+    // Floatinf point  numbers: Float 32 bit, Double 64 bit
+
+    val myFloat: Float = 11.21F
+    val myDouble: Double = 3.1423422
+
+
+    /*
+    var myName = "Elisha"
+    myName   = "heidi"
+    //requires 32 bits of memory
+    // type inference
+    var age = 30
+    print("Hello worlds  " + myName  )
+
+     
+    val mystr = "This is Elisha langat"
+    var myfirstchar = mystr[0]
+    var mylastchar = mystr[mystr.length-1]
+
+    print("this is my $mylastchar and the length of my ${mystr.length} also my first $myfirstchar and I am impressed")
+
+
+    val  a = 6.9
+    val b = 7.2
+    var resultDouble : Double
+    resultDouble = a/b
+    print(resultDouble)
+
+    val isEqual = 5==6
+    println("the values are $isEqual")
+
+    println("the values are greater ${5 >= 10}")
+
+    var P1= 20
+    var P2 = 30
+
+    if (P1>P2){
+        println("use force")
+    }
+    else{
+        println("use technique")
+    }
+
+
+
+
+    var season = 3
+    when (season){
+        1 -> println("Summer")
+        2 -> println("winter")
+        3 ->{
+            println("Fall")
+            println("Autumn")
+        }
+        else -> println("invalid case")
+
+    }
+
+    var month = 3
+    when(month){
+         in 3..5 -> println("spring")
+         in 6..8 -> println("summer")
+         in 9..11 -> println("winter")
+         21,1,2 -> println("fall")
+         else ->println("invalid case")
+
+    }
+
+    var age = 21
+    when (age){
+
+        !in 0..18  -> println("you can now vote")
+        !in 0..21 -> println("you can now drink")
+        else -> println("you are too young")
+    }
+
+    var x = 1
+    while(x <= 10) {
+        println("\n $x ")
+        x++
+
+    }
+    println("\n while loop is done")
+
+
+
+    var x = 100;
+    while (x>=0) {
+
+        println("$x \n")
+        x -= 2
+
+    }
+
+
+    var x =0
+    do {
+        println("$x")
+        x++
+    }while (x<10)
+    println("do while loop is done")
+
+
+
+    var felttemp = "cold"
+    var roomtemp = 5
+
+    while (felttemp == "cold"){
+        //println("room is cold")
+        roomtemp++
+
+        if (roomtemp>=20){
+            felttemp = "comfy"
+            println("Rooom is now warm")
+        }
+    }
+
+
+
+    for (num in 10 downTo 1 step 2){
+        println("$num")
+    }
+
+
+
+    for (i in 1 until 20){
+
+        if (i/2 == 5){
+            continue
+        }
+        println("$i")
+    }
+*/
+}
+
